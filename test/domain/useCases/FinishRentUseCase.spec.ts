0a185a11f1e3547940e382b59d3687e598251013
thiagorf <trfarciroli@gmail.com>
2022-04-01 10:17:51 -0300

    finish rent base structure

---
+++ test/domain/useCases/FinishRentUseCase.spec.ts
@@ -0,0 +1,8 @@
+
+
+describe("Finish Rent Use Case", () => {
+
+    test.todo("Should be able to finish the rent")
+    test.todo("Should not be able to finish a rent that don't exists")
+    test.todo("Should be able to increease the book amount in stock after finish the rent")
+})
