ee060f134a0f4fb522289bf8b8247040a0b0c67a
thiagorf <trfarciroli@gmail.com>
2022-04-18 12:59:19 -0300

    define rate limiter

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,12 +1,22 @@
 import express, {json, NextFunction, Request, Response, } from "express";
+import { rateLimit } from "express-rate-limit";
 import "express-async-errors";
 import { bookRouter } from "./routes/books/book-routes";
 import { rentRouter } from "./routes/rents/rent-routes";
 import { userRouter } from "./routes/users/user-routes";
 
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+	max: 10, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 const app = express();
 
 app.use(json())
+app.use(limiter)
 app.use("/books", bookRouter);
 app.use("/users", userRouter);
 app.use("/rents", rentRouter);
